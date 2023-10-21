e97e753aadf0b55933819c65ea0e532f40bb388e
sbsbsw <sbsbw@byui.edu>
2023-10-21 14:12:42 -0600

    rescued bubble and whiteboard

--- public/participant.js
+++ public/participant.js
@@ -130,11 +130,92 @@ function downloadContentAsImage() {
 downloadContentAsImage();
 
 
+const canvas = document.getElementById("screenshotCanvas");
+const ctx = canvas.getContext('2d');
+const whiteboardButton = document.getElementById("whiteboardButton");
+let whiteboard = false;
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+
+// whiteboard functionallity:
+whiteboardButton.addEventListener('click', () => {
+  if (!whiteboard) {
+    whiteboard = true
+  }
+  else {
+    whiteboard = false
+  } return
+});
+
+function startDrawing(e) {
+  isDrawing = true
+  [lastX, lastY] = [e.clientX, e.clientY]
+}
+
+function draw(e) {
+  if (!isDrawing) return;
+    ctx.beginPath();
+  ctx.strokeStyle = "black";
+  ctx.lineWidth = 2;
+  ctx.moveTo(lastX, lastY);
+  ctx.lineTo(e.clientX, e.clientY);
+  ctx.stroke();
+  [lastX, lastY] = [e.clientX, e.clientY]
+}
+if (whiteboard) {
+canvas.addEventListener('mousedown', startDrawing);
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
+canvas.addEventListener('touchstart', (e) => {
+  e.preventDefault();
+  startDrawing(e.touches[0]);
+});
+canvas.addEventListener('touchmove', (e) => {
+  e.preventDefault();
+  draw(e.touches[0])
+});
+canvas.addEventListener('touched', () =>
+isDrawing = false);
+canvas.addEventListener('touchcancel', () =>
+isDrawing = false);
+}
 
 
 
+// Create a function for the text bubble button.
+const bubbleButton = document.getElementById('bubbleButton');
+bubbleButton.addEventListener('click', () => {
+  const bubble = document.createElement("div");
+  bubble.classname.add('bubble');
+  const content = document.createElement("div");
+  bubble.classname.add('content');
+  canvas.appendchild(bubble)
+  bubble.appendchild(content)
+});
 
+let isDragging = false;
+let initialX;
+let initialY;
 
+const bubble = document.querySelector(".bubble");
+const handle = document.querySelector(".handle");
 
+handle.addEventListener("mousedown", (e) => {
+  isDragging = true;
+  initialX = e.clientX - bubble.getBoundingClientRect().left;
+  initialY = e.clientY - bubble.getBoundingClientRect().top;
+});
 
+document.addEventListener("mousemove", (e) => {
+  if (!isDragging) return;
 
+  const newX = e.clientX - initialX;
+  const newY = e.clientY - initialY;
+  bubble.style.left = newX + "px";
+  bubble.style.top = newY + "px";
+});
+document.addEventListener("mouseup", () => {
+  isDragging = false;
+});
